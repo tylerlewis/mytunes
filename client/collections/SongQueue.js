0a93d0e15f8c5689ca18af996944b7969a46813e
tylerlewis <tyhlewis@gmail.com>
2014-08-06 19:04:49 -0700

    Add functionality to songQueue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,27 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function(){
-  }
+  initialize: function(params){
+    this.on('add', function() {
+      if(this.length === 1){
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function(){
+      this.remove(this.at(0));
+      if(this.length){
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', function() {
+      this.remove(this.at(0));
+    });
+  },
+
+  playFirst: function() {
+    this.models[0].play();
+  },
 
 });
--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -18,7 +18,7 @@ var AppModel = Backbone.Model.extend({
     }, this);
 
     params.library.on('enqueue', function(song){
-      this.attributes.songQueue.push(song);
+      this.attributes.songQueue.add(song);
     }, this);
   }
 
